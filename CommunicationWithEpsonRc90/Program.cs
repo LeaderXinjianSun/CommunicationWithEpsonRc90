b8018b0ca16651af73fe947601265b6b76ba3b4e
sunxinjian <xinjian_sun@sina.com>
2018-01-23 15:49:25 +0800

    init

---
+++ CommunicationWithEpsonRc90/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationWithEpsonRc90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommunicationWithEpsonRc90 communicationWithEpsonRc90 = new CommunicationWithEpsonRc90();
+            Console.ReadKey();
+        }
+    }
+}
